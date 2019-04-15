b8fbd09a720decfa00bdd2b35692278fd73b1a4a
Ediz Ayyıldız <ayyildizediz@gmail.com>
2019-04-15 10:44:39 +0300

    Proje dosyası ekle.

---
+++ WebApiWithAngular/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApiWithAngular.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
